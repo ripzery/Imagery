8cbb0d523e6e48cecee9f33cd2c29f8aa061845a
Phuchit Sirimongkolsathien <phuchit.sir@ascendcorp.com>
2016-11-04 11:36:33 +0700

    Add https module

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var path = require('path');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -14,6 +17,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// This line is from the Node.js HTTPS documentation.
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/ripzery.me/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/ripzery.me/fullchain.pem')
+};
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors());
@@ -23,8 +32,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/photos/', routes);
-app.use('/photos/', image);
+app.use('/imagery/', routes);
+app.use('/imagery/', image);
 
 
 // catch 404 and forward to error handler
@@ -57,6 +66,11 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
+
+// Create an HTTP service.
+http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(443);
 
 
 module.exports = app;
