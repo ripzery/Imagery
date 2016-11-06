82693a6963550df8986d6269afc85a14ca43b3f6
Ripzery <ripzery@gmail.com>
2016-11-06 16:53:12 +0700

    Test

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var options = {
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors());
+// app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
