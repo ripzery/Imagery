75065e124b5e99aa8eed281de3a85b4b41cc0482
Ripzery <ripzery@gmail.com>
2016-11-06 12:40:52 +0700

    test

--- app.js
+++ app.js
@@ -19,13 +19,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // This line is from the Node.js HTTPS documentation.
-// var options = {
-//     key: fs.readFileSync('privkey.pem'),
-//     cert: fs.readFileSync('fullchain.pem')
-// };
+var options = {
+    key: fs.readFileSync('privkey.pem'),
+    cert: fs.readFileSync('fullchain.pem')
+};
 
 // Create an HTTPS service identical to the HTTP service.
-// https.createServer(options, app).listen(443);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -39,36 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(subdomain('api', routes));
 app.use(subdomain('api', image));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-app.set('port', process.env.PORT || 443);
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+https.createServer(options, app).listen(443);
 
 module.exports = app;
